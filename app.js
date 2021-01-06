d8a38f01ecad0b8bafb3212de8dd6ab14b37d278
Avinash <avi.gayam@gmail.com>
2021-01-06 12:29:57 -0600

    Import and Export example

---
+++ app.js
@@ -0,0 +1,4 @@
+//001-Module-ImportExport: Importing functions/variables from importExport.js
+const importExport = require("./001-Module-ImportExport/importExport");
+console.log(`Sum is: ${importExport.sum(1, 2)}`);
+console.log(`Value of Pi is: ${importExport.piValue}`);
